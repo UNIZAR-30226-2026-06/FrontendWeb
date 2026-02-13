6ac97dd109cbd99d79ac37124a9e8fcac344ed52
raultricas <903147@unizar.es>
2026-02-13 01:28:35 +0100

    Primeros ficheros (home, profile aun por hacer)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-function App() {
-  return (
-    <div>
-      <h1>Juego del UNO</h1>
-    </div>
-  );
-}
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Game from "./pages/Game";
+import Profile from "./pages/Profile";
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/game" element={<Game />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
